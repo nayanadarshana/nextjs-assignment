0e39fef5a1c879f3df2c546bacd42e331fed59e4
NAYANA-PC\nayana <nayanadarshana1@gmail.com>
2024-05-22 21:13:11 +0530

    menu update

--- src/app/nav-bar/page.tsx
+++ src/app/nav-bar/page.tsx
@@ -58,7 +58,7 @@ export default function Page() {
                     <path
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                     />
                   </svg>
                 </span>
