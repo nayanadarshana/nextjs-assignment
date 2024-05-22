4b15885e8516f92c36aa882df719700eb28e872b
NAYANA-PC\nayana <nayanadarshana1@gmail.com>
2024-05-22 20:28:15 +0530

    navbar mobile

--- src/app/nav-bar/page.tsx
+++ src/app/nav-bar/page.tsx
@@ -1,32 +1,105 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Page() {
+  const [hide, setHide] = useState<boolean>(false);
+  const hideMenu = () => {
+    setHide(hide ? false : true);
+  };
+
   return (
     <>
       <nav className="navbar">
         <div className="nav-container">
           <ul className="nav-links">
-            <li>
-              <a href="/nav-bar">Aeon</a>
+            <li className="aeon">
+              <a href="/nav-bar">
+                <span className="aeon-text">Aeon</span>
+              </a>
+              {!hide && (
+                <span onClick={hideMenu}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 cancel-icon"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 18 18 6M6 6l12 12"
+                    />
+                  </svg>
+                </span>
+              )}
+              <span onClick={hideMenu}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 hamburger-icon"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+                  />
+                </svg>
+              </span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 search-icon"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  />
+                </svg>
+              </span>
             </li>
-            <li>
-              <a href="/nav-bar">Showcase</a>
-            </li>
-            <li>
-              <a href="/nav-bar">Docs</a>
-            </li>
-            <li>
-              <a href="/nav-bar">Blog</a>
-            </li>
-            <li>
-              <a href="/nav-bar">Analytics</a>
-            </li>
-            <li>
-              <a href="/nav-bar">Template</a>
-            </li>
-            <li>
-              <a href="/nav-bar">Enterprise</a>
-            </li>
+            {/* {!hide && ( */}
+            {/* <div> */}
+            {!hide && (
+              <li className="other-menu">
+                <a href="/nav-bar">Showcase</a>
+              </li>
+            )}
+            {!hide && (
+              <li className="other-menu">
+                <a href="/nav-bar">Docs</a>
+              </li>
+            )}
+            {!hide && (
+              <li className="other-menu">
+                <a href="/nav-bar">Blog</a>
+              </li>
+            )}
+            {!hide && (
+              <li className="other-menu">
+                <a href="/nav-bar">Analytics</a>
+              </li>
+            )}
+            {!hide && (
+              <li className="other-menu">
+                <a href="/nav-bar">Template</a>
+              </li>
+            )}
+            {!hide && (
+              <li className="other-menu">
+                <a href="/nav-bar">Enterprise</a>
+              </li>
+            )}
           </ul>
 
           <form className="search-form">
--- src/app/question3/page.tsx
+++ src/app/question3/page.tsx
@@ -3,12 +3,15 @@ export default function Page() {
     for (let i = 0; i <= numbers.length; i++) {
       for (let j = i + 1; j < numbers.length; j++) {
         if (numbers[i] + numbers[j] === target) {
-          return [numbers.indexOf(numbers[i]), numbers.indexOf(numbers[j])];
+          return [
+            numbers.indexOf(numbers[i]) + 1,
+            numbers.indexOf(numbers[j]) + 1,
+          ];
         }
       }
     }
   }
-  console.log(twoSum([-1, 0], -1));
+  console.log(twoSum([1, 2, 3, 4, 5, 6], 6));
   return (
     <>
       <div></div>
